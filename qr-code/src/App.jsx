4cc31ccdd88918b0a54d8d775fe1695e49171442
Eduardo <ejrb1234@gmail.com>
2024-10-05 13:42:18 -0400

    Project: QR-Code

--- qr-code/src/App.jsx
+++ qr-code/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div style={{ display: 'grid', placeContent: 'center', height: '100dvh' }}>
-      <Card srcImage={'../public/image-qr-code.png'} altImage={'QR'} title={'Improve your front-end skills by building projects'} paragraph={'Scan the QR code to visit Frontend Mentor and take your coding skills to the next level'} />
+      <Card srcImage={'/image-qr-code.png'} altImage={'QR'} title={'Improve your front-end skills by building projects'} paragraph={'Scan the QR code to visit Frontend Mentor and take your coding skills to the next level'} />
     </div>
   )
 }
